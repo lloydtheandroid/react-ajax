4640db8eb01de3a671f1498af1396c9d409f917d
Garrison, Lloyd <lgarrison@def-logix.com>
2018-07-19 08:59:25 -0500

    added axios interceptors to index.js to allow for global logging and error catching

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Can also edit request before returning
+    return request;
+}, error => {
+    // Only handles errors in sending requests
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // Can also edit request before returning
+    return response;
+}, error => {
+    // Handles response errors
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
