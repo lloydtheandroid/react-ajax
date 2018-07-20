48a02ca8f9ef90345b7c493f9cdd2b2d67869c3b
Garrison, Lloyd <lgarrison@def-logix.com>
2018-07-20 13:52:36 -0500

    routing update

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,8 +8,9 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    componentDidUpdate() {
-        if (this.props.id) {
+    componentDidMount() {
+        console.log(this.props);
+        if (this.props.match.params.id) {
             if (!this.state.loadedPost || this.state.loadedPost.id !== this.props.id) {
                 axios.get('posts/' + this.props.id)
                 .then(response => {
