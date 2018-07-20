aa79d7eb093c8e634c57f7782ec338a5287fda1f
Garrison, Lloyd <lgarrison@def-logix.com>
2018-07-20 10:32:59 -0500

    added debug logging

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -9,6 +9,10 @@ class NewPost extends Component {
         content: '',
         author: 'Max'
     }
+    componentDidMount() {
+        console.log(this.props)
+    }
+
     //method
     postDataHandler = () => {
         const postData = {
